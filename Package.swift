682d70d3d88d1820837f56b5920ed195c308d866
Ethan Vaughan <ejvaughan@gmail.com>
2017-07-04 12:28:55 -0500

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftDbus",
     dependencies: [
-        .Package(url: "../Clibdbus", majorVersion: 1)
+        .Package(url: "https://github.com/ejvaughan/Clibdbus", majorVersion: 1)
     ]
 )
